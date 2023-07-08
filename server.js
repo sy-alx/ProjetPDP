72d73c38a592eec2fac8e3d6191111fda338a161
sy-alx <84022109+sy-alx@users.noreply.github.com>
2023-07-08 14:53:56 +0200

    15h
    mise en place des bibliothèque

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+app.use(express.json()); // pour le parsing du JSON
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'utilisateurspdp'
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const query = 'SELECT * FROM utilisateurs WHERE Email = ?';
+  connection.query(query, [email], (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      const user = results[0];
+      bcrypt.compare(password, user.MotDePasse, (err, result) => {
+        if (err) throw err;
+        if (result) {
+          const token = jwt.sign({ id: user.IDutilisateurs }, 'secret_key');
+          res.json({ token });
+        } else {
+          res.status(401).send('Invalid password');
+        }
+      });
+    } else {
+      res.status(401).send('Invalid email');
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
